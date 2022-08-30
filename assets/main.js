287558402a4c5f19ea75d801962a8fbb34242569
k3csk3 <szilagyi.viktor89@gmail.com>
2022-08-30 21:54:37 +0200

    félkész

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+import { images } from './img.js'
+
+const imgPlace = document.querySelector(".container__slide__img");
+const counter = document.querySelector(".img__counter");
+const button = Array.from(document.querySelectorAll(".fa-circle"));
+const newPicture = document.querySelector(".img__arrows--right");
+const previousPicture = document.querySelector(".img__arrows--left");
+const caption = document.querySelector(".img__caption");
+let i = 0;
+
+let createdImg = `<img class= "actualImg" src="./assets/${images[i].source}" alt="picture"></img>`;
+
+let createdDiv = document.createElement('div');
+
+imgPlace.appendChild(createdDiv);
+
+createdDiv.innerHTML = createdImg;
+
+let actualImg = document.querySelector(".actualImg");
+
+newPicture.addEventListener('click', nextPage);
+
+function nextPage() {
+    //  i === images.length -1 ? i = 0 : i++;
+    //  console.log(i);
+      if (i === images.length -1) {
+          i = 0;
+      } else i++;
+
+     actualImg.setAttribute("src", `./assets/${images[i].source}`) 
+}
+
+previousPicture.addEventListener('click', prevPage);
+
+function prevPage() {
+      if (i === 0) {
+          i = 3;
+      } else i--;
+
+     actualImg.setAttribute("src", `./assets/${images[i].source}`) 
+}
