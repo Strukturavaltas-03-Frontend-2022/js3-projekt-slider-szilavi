6e951030e65746e209be60d6945ae87f3221d7db
szilavi <szilagyi.viktor89@gmail.com>
2022-09-04 16:40:29 +0200

    készmajdnem

--- assets/main.js
+++ assets/main.js
@@ -2,11 +2,22 @@ import { images } from './img.js'
 
 const imgPlace = document.querySelector(".container__slide__img");
 const counter = document.querySelector(".img__counter");
-const button = Array.from(document.querySelectorAll(".fa-circle"));
 const newPicture = document.querySelector(".img__arrows--right");
 const previousPicture = document.querySelector(".img__arrows--left");
 const caption = document.querySelector(".img__caption");
+const buttonsContainer = document.querySelector('.container__slide__buttons')
 let i = 0;
+const autoRefresh = 2000
+
+for (let i = 0; i < images.length; i++) {
+    if (i === 0) {
+        buttonsContainer.innerHTML += `<i class="fa-solid fa-circle"></i>`
+    } else {
+        buttonsContainer.innerHTML += `<i class="fa-regular fa-circle"></i>`
+    } 
+}
+
+const buttons = (document.querySelectorAll(".fa-circle")); 
 
 let createdImg = `<img class= "actualImg" src="./assets/${images[i].source}" alt="picture"></img>`;
 
@@ -21,28 +32,52 @@ let actualImg = document.querySelector(".actualImg");
 newPicture.addEventListener('click', nextPage);
 
 function nextPage() {
-    //  i === images.length -1 ? i = 0 : i++;
+    i = i === images.length - 1 ? 0 : i + 1;
     //  console.log(i);
-      if (i === images.length -1) {
-          i = 0;
-      } else i++;
-
-     actualImg.setAttribute("src", `./assets/${images[i].source}`)
-     updateAll()
+    /*
+    if (i === images.length - 1) {
+        i = 0;
+    } else i++;
+*/
+    updateAll()
 }
 
 previousPicture.addEventListener('click', prevPage);
 
 function prevPage() {
-      if (i === 0) {
-          i = 3;
-      } else i--;
-
-     actualImg.setAttribute("src", `./assets/${images[i].source}`)
-     updateAll()
+    i = i === 0 ? images.length - 1 : i - 1;
+    /*if (i === 0) {
+        i = images.length - 1;
+    } else i--;
+    */
+    updateAll()
 };
 
 function updateAll() {
-    counter.innerHTML = `${i+1} / ${images.length}`
+    actualImg.setAttribute("src", `./assets/${images[i].source}`)
+    counter.innerHTML = `${i + 1} / ${images.length}`
     caption.innerHTML = `${images[i].caption}`
+    buttons.forEach(element => {
+        element.classList.remove('fa-solid')
+        element.classList.add('fa-regular')
+    }
+    )
+    buttons[i].classList.add('fa-solid')
+    buttons[i].classList.remove('fa-regular')
+    
 }
+
+function goToPage(page) {
+    i = page
+    updateAll()
+}
+
+buttons.forEach((element, index) => {
+    element.addEventListener('click', ()=> {
+        goToPage(index);
+    })
+});
+
+setInterval(nextPage, autoRefresh);
+
+updateAll()
